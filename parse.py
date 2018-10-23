659cf9ce32070d9b953fc512f4c9f75710ff4d0c
Ryan <inventorryan@gmail.com>
2018-10-23 11:39:27 -0700

    look and take

--- parse.py
+++ parse.py
@@ -3,10 +3,18 @@ def go(dir):
         dir = uinput.split()[1]
         position[0] += nav['{}x'.format(dir.lower())]
         position[1] += nav['{}x'.format(dir.lower())]
-
+def take(item):
+    if uinput.split()[0].lower() == "take":
+        item = uinput.split()[1]
+        if item == items[list(rooms.keys())[list(rooms.values()).index(item)]] #idk i just copied it from https://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary
+            inventory.append(item)
+def look():
+    if uinput.split()[0].lower == 'look':
+        print(descriptions[uinput.split()[1].lower])
 def get():
     uinput = input('what do?\n:')
 
 def parse(eee): #TODO: this makes no sense
-    get()
+    uinput.go()
+    
     
--- yeepsquest.py
+++ yeepsquest.py
@@ -1,36 +1,41 @@
 import random
+from parse import *
 #made by Ryan
 
 #variables
 position = [0,0]
+currentroom
 rooms = {'plains':[0,0],'woods1':[1,0],'shed':[0,1],'creek':[1,1],'dungeonentr':[0,2]}
 mode = 0
 hp = 4
 valids = [[0,0],[1,0],[0,1],[1,1],[0,2]]
 inventory = []
 nav = {'nx':0,'ny':1,'ex':1,'ey':0,'sx':0,'sy':-1,'wx':-1,'wy':0}
-
+items = {'note'}
+sheditems = {'sword', 'key'}
+descriptions = {'plains':"""you stand in the middle of windy plains.\n
+To the north you see a rusty shed, and to the east you see woods.
+\n at your feet you see a note.""",
+                'woods1':
+                
+                
 #commands
-def go(dir):
-    if uinput == "go".lower() + 'N'.lower() or "go".lower() + 'E'.lower() or "go".lower() + 'S'.lower() or "go".lower() + 'W'.lower():
-        dir = uinput.split()[1]
-        position[0] += nav['{}x'.format(dir.lower())]
-        position[1] += nav['{}x'.format(dir.lower())]
-def get():
-    uinput = input('what do?\n:')
+def uproom(): #TODO this
+    curre
+
 #TODO: take, attack, use
 #TODO: hangman? other modes? how do you win?
     
-#parse
-def parse(eee): #TODO: this makes no sense
-    get()
     
 #main loop
 def main():
     print("Welcome to yeep\'s quest! you are yeep, a lone adventurer with flowing golden hair.\n"
-    if mode == 0 or mode == 1:
+    while mode == 0 or mode == 1:
         parse()
-    elif mode == 2:
+        get()
+        take()
+        go()
+    if mode == 2:
           print('GAME OVER\n')
           if input('play again? [Y/N}:').lower() == 'y':
               #TODO: this
@@ -39,3 +44,4 @@ def main():
           break
           
 return 0
+main()
